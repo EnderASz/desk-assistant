8520e68c458c9db8b4c68446d5e5af5518bdd2fa
EnderASz <olek5188@gmail.com>
2023-05-17 08:35:59 +0200

    Add basic plugin register/unregister methods

--- app/desk_assistance/plugin.py
+++ app/desk_assistance/plugin.py
@@ -1,7 +1,41 @@
 class Plugin:
-    ...
+    def __init__(self):
+        self._bound_bearer: "PluginsBearer" | None = None
+
+    @property
+    def bound_bearer(self):
+        return self._bound_bearer
+
+    def register_at(self, bearer: "PluginsBearer"):
+        bearer.register(self)
+
+    def unregister(self):
+        self.bound_bearer.unregister(self)
 
 
 class PluginsBearer:
     def __init__(self):
         self._plugins: list[Plugin] = []
+
+    def register(self, plugin: Plugin):
+        # TODO: Choose politic for "plugin already bound to something"
+        #  - Option 1 - Raise only when plugin bound to another plugin bearer
+        #  - Option 2 - Raise when plugin bound to any plugin bearer
+        if plugin.bound_bearer is not None:
+            # TODO: When chose Option 2 in above TODO remove below condition
+            #  and return
+            if plugin.bound_bearer is self:
+                return
+            raise ...
+
+        self._plugins.append(plugin)
+        plugin.bound_bearer = self
+
+    def unregister(self, plugin: Plugin):
+        if plugin.bound_bearer is not self:
+            # TODO: Consider raising exception (cannot unregister plugin non
+            #  registered at this bearer). This decision should be consistent
+            #  with politic chosen at `PluginBearer.register` method
+            return
+        self._plugins.remove(plugin)
+        plugin.bound_bearer = None
